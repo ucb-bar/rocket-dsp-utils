272cee3c83a31a7496b8f2e933290798e2fb5cac
Jerry Zhao <jerryz123@berkeley.edu>
2024-01-28 23:26:04 -0800

    Update to new RegisterRouter APIs

--- src/main/scala/dspblocks/DspQueue.scala
+++ src/main/scala/dspblocks/DspQueue.scala
@@ -7,111 +7,86 @@ import freechips.rocketchip.amba.apb._
 import freechips.rocketchip.amba.axi4._
 import freechips.rocketchip.amba.axi4stream.AXI4StreamIdentityNode
 import org.chipsalliance.cde.config.Parameters
-import freechips.rocketchip.diplomacy.LazyModuleImp
+import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.regmapper._
 import freechips.rocketchip.tilelink._
+import freechips.rocketchip.interrupts._
 
-trait DspQueue[D, U, EO, EI, B <: Data] extends DspBlock[D, U, EO, EI, B] {
+trait DspQueue[D, U, EO, EI, B <: Data] extends DspBlock[D, U, EO, EI, B] { this: RegisterRouter with HasInterruptSources =>
 
   /**
     * Depth of queue
     */
   val depth: Int
 
+  def nInterrupts = 2
+
   require(depth > 0)
 
   override val streamNode: AXI4StreamIdentityNode = AXI4StreamIdentityNode()
-}
 
-trait DspQueueImp[D, U, EO, EI, B <: Data] extends LazyModuleImp with HasRegMap {
-  def outer: DspQueue[D, U, EO, EI, B] = wrapper.asInstanceOf[DspQueue[D, U, EO, EI, B]]
-  val streamNode = outer.streamNode
-  val depth = outer.depth
+  override lazy val module = new LazyModuleImp(this) {
+    val (streamIn, streamEdgeIn) = streamNode.in.head
+    val (streamOut, streamEdgeOut) = streamNode.out.head
 
-  val (streamIn, streamEdgeIn) = streamNode.in.head
-  val (streamOut, streamEdgeOut) = streamNode.out.head
+    val queuedStream = Queue(streamIn, entries = depth)
+    streamOut <> queuedStream
 
-  val queuedStream = Queue(streamIn, entries = depth)
-  streamOut <> queuedStream
+    val queueEntries = RegInit(UInt(log2Ceil(depth + 1).W), 0.U)
+    queueEntries := queueEntries + streamIn.fire - streamOut.fire
 
-  val queueEntries = RegInit(UInt(log2Ceil(depth + 1).W), 0.U)
-  queueEntries := queueEntries + streamIn.fire - streamOut.fire
+    val queueThreshold = WireInit(UInt(64.W), depth.U)
+    val queueFilling = queueEntries >= queueThreshold
+    val queueFull = queueEntries >= depth.U
 
-  val queueThreshold = WireInit(UInt(64.W), depth.U)
-  val queueFilling = queueEntries >= queueThreshold
-  val queueFull = queueEntries >= depth.U
+    interrupts := VecInit(queueFilling, queueFull)
 
-  override val interrupts: Vec[Bool] = VecInit(queueFilling, queueFull)
-  regmap(
-    0 ->
-      Seq(
-        RegField(
-          64,
-          queueThreshold,
-          RegFieldDesc("queueThreshold", "Threshold for number of elements to throw interrupt")
+    regmap(
+      0 ->
+        Seq(
+          RegField(
+            64,
+            queueThreshold,
+            RegFieldDesc("queueThreshold", "Threshold for number of elements to throw interrupt")
+          )
         )
-      )
-  )
-
+    )
+  }
 }
 
 class TLDspQueue(val depth: Int, val baseAddr: BigInt = 0, devname: String = "vqueue", concurrency: Int = 1)(implicit
   p:                        Parameters
-) extends TLRegisterRouter(baseAddr, devname, Seq("ucb-bar,vreg"), beatBytes = 8, concurrency = concurrency)(
-      new TLRegBundle(depth, _)
-    )(
-      new TLRegModule(depth, _, _)
-        with DspQueueImp[TLClientPortParameters, TLManagerPortParameters, TLEdgeOut, TLEdgeIn, TLBundle]
-    )
+) extends RegisterRouter(RegisterRouterParams(devname, Nil, baseAddr, beatBytes=8, concurrency=concurrency))(p)
+    with HasTLControlRegMap
+    with HasInterruptSources
     with DspQueue[TLClientPortParameters, TLManagerPortParameters, TLEdgeOut, TLEdgeIn, TLBundle]
     with TLDspBlock {
-  override val mem = Some(node)
+  val mem = Some(controlNode)
 }
 
 class AXI4DspQueue(val depth: Int, val baseAddr: BigInt = 0, concurrency: Int = 4)(implicit p: Parameters)
-    extends AXI4RegisterRouter(baseAddr, beatBytes = 8, concurrency = concurrency)(new AXI4RegBundle(depth, _))(
-      new AXI4RegModule(depth, _, _)
-        with DspQueueImp[
-          AXI4MasterPortParameters,
-          AXI4SlavePortParameters,
-          AXI4EdgeParameters,
-          AXI4EdgeParameters,
-          AXI4Bundle
-        ]
-    )
+    extends RegisterRouter(RegisterRouterParams("vqueue", Nil, baseAddr, beatBytes=8, concurrency=concurrency))(p)
+    with HasAXI4ControlRegMap
+    with HasInterruptSources
     with DspQueue[AXI4MasterPortParameters, AXI4SlavePortParameters, AXI4EdgeParameters, AXI4EdgeParameters, AXI4Bundle]
     with AXI4DspBlock {
-  val mem = Some(node)
+  val mem = Some(controlNode)
 }
 
 class AHBDspQueue(val depth: Int, val baseAddr: BigInt = 0, concurrency: Int = 4)(implicit p: Parameters)
-    extends AHBRegisterRouter(baseAddr, beatBytes = 8, concurrency = concurrency)(new AHBRegBundle(depth, _))(
-      new AHBRegModule(depth, _, _)
-        with DspQueueImp[
-          AHBMasterPortParameters,
-          AHBSlavePortParameters,
-          AHBEdgeParameters,
-          AHBEdgeParameters,
-          AHBSlaveBundle
-        ]
-    )
+    extends RegisterRouter(RegisterRouterParams("vqueue", Nil, baseAddr, beatBytes=8, concurrency=concurrency))(p)
+    with HasAHBControlRegMap
+    with HasInterruptSources
     with DspQueue[AHBMasterPortParameters, AHBSlavePortParameters, AHBEdgeParameters, AHBEdgeParameters, AHBSlaveBundle]
     with AHBSlaveDspBlock {
-  val mem = Some(node)
+  val mem = Some(controlNode)
 }
 
 class APBDspQueue(val depth: Int, val baseAddr: BigInt = 0, concurrency: Int = 4)(implicit p: Parameters)
-    extends APBRegisterRouter(baseAddr, beatBytes = 8, concurrency = concurrency)(new APBRegBundle(depth, _))(
-      new APBRegModule(depth, _, _)
-        with DspQueueImp[
-          APBMasterPortParameters,
-          APBSlavePortParameters,
-          APBEdgeParameters,
-          APBEdgeParameters,
-          APBBundle
-        ]
-    )
+    extends RegisterRouter(RegisterRouterParams("vqueue", Nil, baseAddr, beatBytes=8, concurrency=concurrency))
+    with HasAPBControlRegMap
+    with HasInterruptSources
     with DspQueue[APBMasterPortParameters, APBSlavePortParameters, APBEdgeParameters, APBEdgeParameters, APBBundle]
     with APBDspBlock {
-  val mem = Some(node)
+  val mem = Some(controlNode)
 }
--- src/main/scala/dspblocks/DspRegister.scala
+++ src/main/scala/dspblocks/DspRegister.scala
@@ -8,8 +8,9 @@ import org.chipsalliance.cde.config.Parameters
 import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.regmapper._
 import freechips.rocketchip.tilelink._
+import freechips.rocketchip.interrupts._
 
-trait DspRegister[D, U, EO, EI, B <: Data] extends DspBlock[D, U, EO, EI, B] {
+trait DspRegister[D, U, EO, EI, B <: Data] extends DspBlock[D, U, EO, EI, B] { this: RegisterRouter with HasInterruptSources =>
 
   /**
     * Maximum length of the vector register
@@ -23,133 +24,131 @@ trait DspRegister[D, U, EO, EI, B <: Data] extends DspBlock[D, U, EO, EI, B] {
 
   require(len > 0)
 
+  def nInterrupts = 0
+
   override val streamNode: AXI4StreamIdentityNode = AXI4StreamIdentityNode()
-}
 
-trait DspRegisterImp[D, U, EO, EI, B <: Data] extends LazyModuleImp with HasRegMap {
-  def outer: DspRegister[D, U, EO, EI, B] = wrapper.asInstanceOf[DspRegister[D, U, EO, EI, B]]
-  val maxlen = outer.len
-  val mapMem = outer.mapMem
-  val streamNode = outer.streamNode
+  override lazy val module = new LazyModuleImp(this) {
+    val maxlen = len
 
-  val (streamIn, streamEdgeIn) = streamNode.in.head
-  val (streamOut, streamEdgeOut) = streamNode.out.head
+    val (streamIn, streamEdgeIn) = streamNode.in.head
+    val (streamOut, streamEdgeOut) = streamNode.out.head
 
-  require(streamEdgeIn.bundle.hasData, "DspRegister registers data, must have data field!")
-  require(!streamEdgeIn.bundle.hasKeep, "DspRegister does not support TKEEP")
-  require(!streamEdgeIn.bundle.hasStrb, "DspRegister does not support TSTRB")
-  require(streamEdgeIn.bundle.i == 0 && streamEdgeIn.bundle.d == 0, "DspRegister does not support TID or TDEST")
+    require(streamEdgeIn.bundle.hasData, "DspRegister registers data, must have data field!")
+    require(!streamEdgeIn.bundle.hasKeep, "DspRegister does not support TKEEP")
+    require(!streamEdgeIn.bundle.hasStrb, "DspRegister does not support TSTRB")
+    require(streamEdgeIn.bundle.i == 0 && streamEdgeIn.bundle.d == 0, "DspRegister does not support TID or TDEST")
 
-  val protoData = UInt(streamIn.params.dataBits.W)
-  val mem = SyncReadMem(maxlen, protoData)
-  val user = Reg(protoData)
+    val protoData = UInt(streamIn.params.dataBits.W)
+    val mem = SyncReadMem(maxlen, protoData)
+    val user = Reg(protoData)
 
-  val veclen = RegInit(maxlen.U)
+    val veclen = RegInit(maxlen.U)
 
-  // Load is reading from register to streamOut
-  val loading = RegInit(false.B)
-  // Store is writing to register from streamIn
-  val storing = RegInit(false.B)
+    // Load is reading from register to streamOut
+    val loading = RegInit(false.B)
+    // Store is writing to register from streamIn
+    val storing = RegInit(false.B)
 
-  val loadAfterStore = RegInit(false.B)
-  val storeAfterLoad = !loadAfterStore
+    val loadAfterStore = RegInit(false.B)
+    val storeAfterLoad = !loadAfterStore
 
-  val loadIdx = Reg(UInt(log2Ceil(maxlen).W))
-  val storeIdx = Reg(UInt(log2Ceil(maxlen).W))
+    val loadIdx = Reg(UInt(log2Ceil(maxlen).W))
+    val storeIdx = Reg(UInt(log2Ceil(maxlen).W))
 
-  when(!loading) { loadIdx := 0.U }
-  when(!storing) { storeIdx := 0.U }
+    when(!loading) { loadIdx := 0.U }
+    when(!storing) { storeIdx := 0.U }
 
-  val (memMap, memReadEn, memWriteEn) =
-    MemToRegmap(streamIn.bits.data.cloneType, mem, true.B, true.B, 24, wordSizeBytes = Some(8))
+    val (memMap, memReadEn, memWriteEn) =
+      MemToRegmap(streamIn.bits.data.cloneType, mem, true.B, true.B, 24, wordSizeBytes = Some(8))
 
-  val loadOK = (!storing || storeAfterLoad || (loadIdx < storeIdx)) && !memReadEn
-  val storeOK = (!loading || loadAfterStore || (storeIdx < loadIdx)) && !memWriteEn
+    val loadOK = (!storing || storeAfterLoad || (loadIdx < storeIdx)) && !memReadEn
+    val storeOK = (!loading || loadAfterStore || (storeIdx < loadIdx)) && !memWriteEn
 
-  val readIdx = WireInit(loadIdx)
-  val readEn = !memReadEn
-  val readMem = mem.read(readIdx) //, readEn)
+    val readIdx = WireInit(loadIdx)
+    val readEn = !memReadEn
+    val readMem = mem.read(readIdx) //, readEn)
 
-  val writeEn = WireInit(false.B)
-  val writeIdx = WireInit(UInt(), 0.U)
-  val writeVal = WireInit(UInt(), 0.U)
-  when(writeEn) {
-    mem.write(writeIdx, writeVal)
-  }
-
-  streamOut.bits.data := readMem
-  when(!readEn) {
-    streamOut.bits.data := 0.U
-  }
-  streamOut.bits.user := user
-  streamOut.bits.last := loadIdx === (veclen - 1.U)
-  streamOut.valid := loading && loadOK
-
-  when(loading && streamOut.fire) {
-    val next = loadIdx +& 1.U
-    readIdx := next
-    when(next === veclen) {
-      loadIdx := 0.U
-      readIdx := 0.U
-      loading := false.B
-    }.otherwise {
-      loadIdx := next
-      loading := true.B
+    val writeEn = WireInit(false.B)
+    val writeIdx = WireInit(UInt(), 0.U)
+    val writeVal = WireInit(UInt(), 0.U)
+    when(writeEn) {
+      mem.write(writeIdx, writeVal)
     }
-  }
 
-  streamIn.ready := storing && storeOK //loading && loadOK
-
-  when(storing && streamIn.fire) {
-    when(writeIdx === 0.U) {
-      user := streamIn.bits.user
+    streamOut.bits.data := readMem
+    when(!readEn) {
+      streamOut.bits.data := 0.U
     }
-    writeEn := true.B
-    writeIdx := storeIdx
-    writeVal := streamIn.bits.data
+    streamOut.bits.user := user
+    streamOut.bits.last := loadIdx === (veclen - 1.U)
+    streamOut.valid := loading && loadOK
 
-    val next = storeIdx +& 1.U
-    when(next === veclen) {
-      storeIdx := 0.U
-      storing := false.B
-    }.otherwise {
-      storeIdx := next
-      storing := true.B
+    when(loading && streamOut.fire) {
+      val next = loadIdx +& 1.U
+      readIdx := next
+      when(next === veclen) {
+        loadIdx := 0.U
+        readIdx := 0.U
+        loading := false.B
+      }.otherwise {
+        loadIdx := next
+        loading := true.B
+      }
     }
-  }
 
-  when(!loading && storing) {
-    loadAfterStore := true.B
-  }
-  when(!storing) {
-    loadAfterStore := false.B
-  }
+    streamIn.ready := storing && storeOK //loading && loadOK
 
-  regmap(
-    Seq(
-      0 -> Seq(
-        RegField(
-          64,
-          RegReadFn(veclen),
-          RegWriteFn((valid, data) => {
-            when(valid) {
-              veclen := Mux(data > maxlen.U, maxlen.U, data)
-            }
-            true.B
-          }),
-          RegFieldDesc("veclen", "Vector length register (if writing > len, sets to len)")
+    when(storing && streamIn.fire) {
+      when(writeIdx === 0.U) {
+        user := streamIn.bits.user
+      }
+      writeEn := true.B
+      writeIdx := storeIdx
+      writeVal := streamIn.bits.data
+
+      val next = storeIdx +& 1.U
+      when(next === veclen) {
+        storeIdx := 0.U
+        storing := false.B
+      }.otherwise {
+        storeIdx := next
+        storing := true.B
+      }
+    }
+
+    when(!loading && storing) {
+      loadAfterStore := true.B
+    }
+    when(!storing) {
+      loadAfterStore := false.B
+    }
+
+    regmap(
+      Seq(
+        0 -> Seq(
+          RegField(
+            64,
+            RegReadFn(veclen),
+            RegWriteFn((valid, data) => {
+              when(valid) {
+                veclen := Mux(data > maxlen.U, maxlen.U, data)
+              }
+              true.B
+            }),
+            RegFieldDesc("veclen", "Vector length register (if writing > len, sets to len)")
+          )
+        ),
+        8 -> Seq(
+          RegField(8, loading, RegFieldDesc("streamLoading", "Load from AXI-4 Stream")),
+          RegField(8 + 48, storing, RegFieldDesc("streamStoring", "Store to AXI-4 Stream"))
+        ),
+        16 -> Seq(
+          RegField(64, user, RegFieldDesc("user", "TUSER"))
         )
-      ),
-      8 -> Seq(
-        RegField(8, loading, RegFieldDesc("streamLoading", "Load from AXI-4 Stream")),
-        RegField(8 + 48, storing, RegFieldDesc("streamStoring", "Store to AXI-4 Stream"))
-      ),
-      16 -> Seq(
-        RegField(64, user, RegFieldDesc("user", "TUSER"))
-      )
-    ) ++ (if (mapMem) memMap else Seq()): _*
-  )
-
+      ) ++ (if (mapMem) memMap else Seq()): _*
+    )
+  }
 }
 
 class TLDspRegister(
@@ -159,29 +158,19 @@ class TLDspRegister(
   devname:      String = "vreg",
   concurrency:  Int = 1
 )(implicit p:   Parameters)
-    extends TLRegisterRouter(baseAddr, devname, Seq("ucb-bar,vreg"), beatBytes = 8, concurrency = concurrency)(
-      new TLRegBundle(len, _)
-    )(
-      new TLRegModule(len, _, _)
-        with DspRegisterImp[TLClientPortParameters, TLManagerPortParameters, TLEdgeOut, TLEdgeIn, TLBundle]
-    )
+    extends RegisterRouter(RegisterRouterParams(devname, Seq("ucb-bar,vreg"), baseAddr, beatBytes=8, concurrency=concurrency))(p)
+    with HasTLControlRegMap
+    with HasInterruptSources
     with DspRegister[TLClientPortParameters, TLManagerPortParameters, TLEdgeOut, TLEdgeIn, TLBundle]
     with TLDspBlock {
-  override val mem = Some(node)
+  override val mem = Some(controlNode)
 }
 
 class AXI4DspRegister(val len: Int, val mapMem: Boolean = true, val baseAddr: BigInt = 0, concurrency: Int = 4)(implicit
   p:                           Parameters
-) extends AXI4RegisterRouter(baseAddr, beatBytes = 8, concurrency = concurrency)(new AXI4RegBundle(len, _))(
-      new AXI4RegModule(len, _, _)
-        with DspRegisterImp[
-          AXI4MasterPortParameters,
-          AXI4SlavePortParameters,
-          AXI4EdgeParameters,
-          AXI4EdgeParameters,
-          AXI4Bundle
-        ]
-    )
+) extends RegisterRouter(RegisterRouterParams("vreg", Seq("ucb-bar,vreg"), baseAddr, beatBytes=8, concurrency=concurrency))(p)
+    with HasAXI4ControlRegMap
+    with HasInterruptSources
     with DspRegister[
       AXI4MasterPortParameters,
       AXI4SlavePortParameters,
@@ -190,5 +179,5 @@ class AXI4DspRegister(val len: Int, val mapMem: Boolean = true, val baseAddr: Bi
       AXI4Bundle
     ]
     with AXI4DspBlock {
-  val mem = Some(node)
+  val mem = Some(controlNode)
 }
